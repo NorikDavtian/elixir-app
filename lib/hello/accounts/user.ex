b02a40222cd00f465f5a800a1dc7c649a958a546
Norik Davtian <norik@joinboulevard.com>
2019-06-20 17:00:11 -0700

    crud with user and credentials relationship

--- lib/hello/accounts/user.ex
+++ lib/hello/accounts/user.ex
@@ -1,10 +1,12 @@
 defmodule Hello.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Hello.Accounts.Credential
 
   schema "users" do
-    field :email, :string
     field :name, :string
+    field :username, :string
+    has_one :credential, Credential
 
     timestamps()
   end
@@ -12,8 +14,8 @@ defmodule Hello.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email])
-    |> validate_required([:name, :email])
-    |> unique_constraint(:email)
+    |> cast(attrs, [:name, :username])
+    |> validate_required([:name, :username])
+    |> unique_constraint(:username)
   end
 end
